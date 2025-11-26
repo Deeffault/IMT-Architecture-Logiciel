f753f160f647c0f2063ef475a9501adde867f664
Théo Lebiez <theo.lebiez@etu.imt-nord-europe.fr>
2025-11-26 17:35:06 +0100

    fix: use static dto instead of mappers in clients

--- adapters-in-rest/src/main/java/com/imt/adaptersinrest/clients/model/input/ClientUpdateInput.java
+++ adapters-in-rest/src/main/java/com/imt/adaptersinrest/clients/model/input/ClientUpdateInput.java
@@ -2,23 +2,38 @@ package com.imt.adaptersinrest.clients.model.input;
 
 import com.imt.adaptersinrest.common.model.input.AbstractUpdateInput;
 import com.imt.adaptersinrest.common.model.input.UpdatableProperty;
+import com.imt.clients.model.Client;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.io.Serial;
+import java.time.LocalDate;
 
 /**
  * DTO pour la mise à jour partielle (PATCH) d'un client.
- * Utilise UpdatableProperty pour chaque champ.
  */
 @Getter
 @Setter
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class ClientUpdateInput extends AbstractUpdateInput {
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     private UpdatableProperty<String> lastName = UpdatableProperty.empty();
     private UpdatableProperty<String> firstName = UpdatableProperty.empty();
-    private UpdatableProperty<String> dateOfBirth = UpdatableProperty.empty();
+    private UpdatableProperty<LocalDate> dateOfBirth = UpdatableProperty.empty();
     private UpdatableProperty<String> adress = UpdatableProperty.empty();
 
-    // On ne permet pas la mise à jour du numéro de permis (identifiant métier)
-    // private UpdatableProperty<String> licenseNumber = UpdatableProperty.empty();
-
+    // PATCH conversion
+    public static Client from(final ClientUpdateInput input, final Client existingClient) {
+        return existingClient.toBuilder()
+                .lastName(input.getLastName().defaultIfNotOverwrite(existingClient.getLastName()))
+                .firstName(input.getFirstName().defaultIfNotOverwrite(existingClient.getFirstName()))
+                .dateOfBirth(input.getDateOfBirth().defaultIfNotOverwrite(existingClient.getDateOfBirth()))
+                .adress(input.getAdress().defaultIfNotOverwrite(existingClient.getAdress()))
+                .build();
+    }
 }
